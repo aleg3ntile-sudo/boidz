441415a479069b54995bbf29ab5eeb8a68c64fc3
Alessandro gentile <ale@pop-os.home-life.hub>
2026-05-11 20:37:05 +0200

    test minimale e stuct mock per dati riutilizzabili

--- boidz.hpp
+++ boidz.hpp
@@ -1,67 +1,60 @@
-#include <vector>
-#include <algorithm>
 #include "rules.hpp"
+#include <algorithm>
+#include <vector>
 
 #ifndef BOIDZ_HPP
 #define BOIDZ_HPP
 
-namespace boidz
-{
-    struct Coords
-    {
-        double x;
-        double y;
-    };
+namespace boidz {
+struct Coords {
+  double x;
+  double y;
+};
 
-    Coords operator+(const Coords &p, const Coords &q)
-    {
-        return {p.x + q.x, p.y + q.y};
-    }
-
-    struct Parameters
-    {
-        double s;   // parametro di separazione
-        double d;   // parametro vicini
-        double d_s; // paramtero vicini critici
-        double a;   // parametro di allineamento
-        double c;   // parametro di coesione
-    };
-    class Flock;
-    class SingleBoid
-    {
-        Coords position;
-        Coords velocity;
-        int cardinality; // e che nessuno osi fiatare
-
-    public:
-        SingleBoid() : position{}, velocity{}, cardinality(0) {}
-        SingleBoid(Coords p, Coords v, int i) : position(p), velocity(v), cardinality(i) {}
-        auto getCardinality() { return cardinality; }
-        auto getPosition() { return position; }
-        auto getVelocity() { return velocity; }
-
-        void update_Position_in_time_for_single_boid(double time)
-        {
-            position.x = position.x + time * velocity.x;
-            position.y = position.y + time * velocity.y;
-        }
-
-        void update_Velocity_with_rules_for_single_boid(Coords updated_velocity)
-        {
-           velocity = velocity + updated_velocity;
-        }
-        // Coords update_Velocity_in_time_for_single_boid() {}
-    };
-
-    class Flock
-    {
-
-    public:
-        std::vector<SingleBoid> flock{};
-        Flock() : flock{} {}
-        Flock(int N) : flock(N) {}
-    };
-
+Coords operator+(const Coords &p, const Coords &q) {
+  return {p.x + q.x, p.y + q.y};
 }
+
+struct Parameters {
+  double s;   // parametro di separazione
+  double d;   // parametro vicini
+  double d_s; // paramtero vicini critici
+  double a;   // parametro di allineamento
+  double c;   // parametro di coesione
+};
+
+class SingleBoid {
+  Coords position;
+  Coords velocity;
+  int cardinality; // e che nessuno osi fiatare
+
+public:
+  SingleBoid() : position{}, velocity{}, cardinality(0) {}
+  SingleBoid(Coords p, Coords v, int i)
+      : position(p), velocity(v), cardinality(i) {}
+  auto getCardinality() { return cardinality; }
+  auto getPosition() { return position; }
+  auto getVelocity() { return velocity; }
+
+  void update_Position_in_time_for_single_boid(double time) {
+    position.x = position.x + time * velocity.x;
+    position.y = position.y + time * velocity.y;
+  }
+
+  void update_Velocity_with_rules_for_single_boid(Coords updated_velocity) {
+    velocity = velocity + updated_velocity;
+  }
+  // Coords update_Velocity_in_time_for_single_boid() {}
+};
+
+class Flock {
+
+public:
+  std::vector<SingleBoid> flock{};
+  Flock() : flock{} {}
+  Flock(int N) : flock(N) {}
+};
+
+} // namespace boidz
 
 #endif
--- test.cpp
+++ test.cpp
@@ -1,3 +1,24 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
+#include "rules.hpp"
+using namespace boidz;
+// struct con elementi utilizzabili in più test per non doverli ricreare ogni
+// volta
+struct MocksFlock {
+  Flock stormo_vuoto;
+  Flock solitario{1};
+  Flock mock_stormo{10};
+};
 
+struct MocksBoid {
+  SingleBoid uccello_fermo;
+};
+
+struct MocksPar {};
+
+TEST_CASE("check get_distance function ") {
+  SingleBoid mock_bird_1{{1.0, 2.0}, {0.5, 0.5}, 3};
+  SingleBoid mock_bird_2{{3.0, 1.5}, {1.0, 0.2}, 12};
+  CHECK(get_distance(mock_bird_1, mock_bird_2) ==
+        doctest::Approx(2.06).epsilon(0.01));
+}
