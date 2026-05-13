9f4cd6f74c004a3a14266612ebf23f7747387939
ale <aleg3ntile@gmail.com>
2026-05-13 09:40:44 +0200

    aggiunti test

--- main.cpp
+++ main.cpp
@@ -4,20 +4,18 @@
 
 using namespace boidz;
 
-int main()
-{
-    Flock stormo{};
-    std::random_device r;
-    std::default_random_engine eng(r());
-    std::uniform_real_distribution<double> uniform{0., 1.};
+int main() {
+  Flock stormo{};
+  std::random_device r;
+  std::default_random_engine eng(r());
+  std::uniform_real_distribution<double> uniform{0., 1.};
 
-    for (int j{0}; j != 10; ++j)
-    {
-        Coords p{uniform(eng), uniform(eng)};
-        Coords v{uniform(eng), uniform(eng)};
-        SingleBoid b = SingleBoid(p, v, j);
-        stormo.flock.push_back(b);
-    }
+  for (int j{0}; j != 10; ++j) {
+    Coords p{uniform(eng), uniform(eng)};
+    Coords v{uniform(eng), uniform(eng)};
+    SingleBoid b = SingleBoid(p, v, j);
+    stormo.flock.push_back(b);
+  }
 
-    check_status(stormo);
+  check_status(stormo);
 }
--- test.cpp
+++ test.cpp
@@ -2,8 +2,8 @@
 #include "doctest.h"
 #include "rules.hpp"
 using namespace boidz;
-// struct con elementi utilizzabili in più test per non doverli ricreare ogni
-// volta
+/* struct con elementi utilizzabili in più test per non doverli ricreare ogni
+volta
 struct MocksFlock {
   Flock stormo_vuoto;
   Flock solitario{1};
@@ -14,11 +14,50 @@ struct MocksBoid {
   SingleBoid uccello_fermo;
 };
 
-struct MocksPar {};
+struct MocksPar {}; */
 
-TEST_CASE("check get_distance function ") {
-  SingleBoid mock_bird_1{{1.0, 2.0}, {0.5, 0.5}, 3};
-  SingleBoid mock_bird_2{{3.0, 1.5}, {1.0, 0.2}, 12};
-  CHECK(get_distance(mock_bird_1, mock_bird_2) ==
-        doctest::Approx(2.06).epsilon(0.01));
+TEST_CASE("check get_distance function") {
+  SingleBoid b1{{1.0, 2.0}, {0.5, 0.5}, 3};
+  SingleBoid b2{{3.0, 1.5}, {1.0, 0.2}, 12};
+  CHECK(get_distance(b1, b2) == doctest::Approx(2.06).epsilon(0.01));
+}
+
+TEST_CASE("check check_critical_distance function") {
+  SingleBoid b1{{0.0, 0.0}, {0.0, 0.0}, 1};
+  SingleBoid b2{{2.0, 0.0}, {0.0, 0.0}, 1};
+  CHECK(check_critical_distance(2.0, b1, b2) == true);
+  CHECK(check_critical_distance(1.0, b1, b2) == false);
+}
+
+TEST_CASE("check check_neighbours function") {
+  SingleBoid b1{{0.0, 0.0}, {0.0, 0.0}, 1};
+  SingleBoid b2{{3.0, 4.0}, {0.0, 0.0}, 1};
+  CHECK(check_neighbours(5.0, b1, b2) == true);
+  CHECK(check_neighbours(4.9, b1, b2) == false);
+}
+
+TEST_CASE("check get_neighbours function - no neighbours") {
+  Flock f;
+  SingleBoid b1{{0.0, 0.0}, {0.0, 0.0}, 1};
+  SingleBoid b2{{10.0, 0.0}, {0.0, 0.0}, 2};
+  SingleBoid b3{{20.0, 0.0}, {0.0, 0.0}, 3};
+  f.flock.push_back(b1);
+  f.flock.push_back(b2);
+  f.flock.push_back(b3);
+
+  auto result = get_neighbours(2.0, b1, f);
+  CHECK(result.size() == 0);
+}
+
+TEST_CASE("check get_neighbours function - some neighbours") {
+  Flock f;
+  SingleBoid b1{{0.0, 0.0}, {0.0, 0.0}, 1};
+  SingleBoid b2{{1.0, 0.0}, {0.0, 0.0}, 2};
+  SingleBoid b3{{10.0, 0.0}, {0.0, 0.0}, 3};
+  f.flock.push_back(b1);
+  f.flock.push_back(b2);
+  f.flock.push_back(b3);
+
+  auto result = get_neighbours(2.0, b1, f);
+  CHECK(result.size() == 1);
 }
