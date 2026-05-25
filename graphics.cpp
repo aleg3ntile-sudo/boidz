ed4949bd90bd51d293c6cedab0136d5496658ad8
Marcello1501 <marcello.bughi@studio.unibo.it>
2026-05-25 16:59:54 +0200

    graphics is not working as of 25/05/26 17:00

--- graphics.cpp
+++ graphics.cpp
@@ -3,18 +3,20 @@
 
 #include "boidz.hpp"
 #include "rules.hpp"
+#include "stats.cpp"
+#include <cmath>
 
 // ipotetico main smfl
 using namespace boidz;
 int main() {
   sf::RenderWindow window(
-      sf::VideoMode(800, 600),   // inizializzerò poi height e width (o lasciamo
-                                 // sempre fissi 600-800?)
-      "Boids");                  // informazioni sulla finestra grafica
-  window.setFramerateLimit(60);  // fps
+      sf::VideoMode(800, 600),  // inizializzerò poi height e width (o lasciamo
+                                // sempre fissi 600-800?)
+      "Boids");                 // informazioni sulla finestra grafica
+  window.setFramerateLimit(60); // fps
 
-  // sf::Texture texture; // creiamo una texture
-  // texture.loadFromFile("bobsenzasfondo.png");
+  sf::RenderWindow window2(sf::VideoMode(800, 600), "Statistics");
+  window.setFramerateLimit(60);
 
   sf::CircleShape dot(1.f);
   dot.setFillColor(sf::Color::Red);
@@ -46,59 +48,95 @@ int main() {
   Parameters params{1.2, 50.0, 7.5, 0.3, 0.02, 1., 0.5};
 
   sf::Clock clock;
-  while (window.isOpen()) {
-    double dt = clock.restart().asSeconds();
 
-    sf::Event event;  // tiene la finestra viva anche se non crrreiamo eventi,
-                      // poll event evita che la finestra si blocchi
+  float margine_x = 50.f;
+  float margine_y = 50.f;
+
+  sf::CircleShape point(2.f);
+  point.setFillColor(sf::Color::Red);
+
+  sf::Vector2f origin(margine_x, 600.f - margine_y);
+
+  sf::VertexArray axis(sf::PrimitiveType::Lines, 4);
+  sf::Color axis_color = sf::Color::White;
+
+  axis[0].position = origin;
+  axis[0].color = axis_color;
+  axis[1].position = sf::Vector2f(800.f - 50.f, origin.y);
+  axis[1].color = axis_color;
+
+  axis[2].position = origin;
+  axis[2].color = axis_color;
+  axis[3].position = sf::Vector2f(origin.x, 50.f);
+  axis[3].color = axis_color;
+
+  std::vector<Coords> vector_of_mean_speeds{};
+
+  while (window.isOpen() || window2.isOpen()) {
+
+    sf::Event event;
     while (window.pollEvent(event)) {
+
       if (event.type == sf::Event::Closed) {
         window.close();
+      } else {
+        
+        double dt = clock.restart().asSeconds();
+        std::vector<Coords> updated_velocities{};
+        for (auto &x : stormo.flock) {
+          Coords new_velocity = create_velocity_with_rules_for_single_boid(
+              params, stormo, x, hunter);
+          updated_velocities.push_back(new_velocity);
+        }
+        for (long unsigned int i = 0; i != stormo.flock.size(); ++i) {
+          stormo.flock[i].update_Velocity_with_rules_for_single_boid(
+              updated_velocities[i]);
+          stormo.flock[i].update_Position_in_time_for_single_boid(dt);
+          stormo.flock[i].BorderRestriction(800., 600.);
+          stormo.flock[i].SpeedRestriction(130);
+          stormo.flock[i].aggiornaSprite();
+        }
+
+        Coords new_hunt_velocity = hunt(hunter, stormo, params);
+        hunter.update_Velocity_with_rules_for_single_boid(new_hunt_velocity);
+        hunter.update_Position_in_time_for_single_boid(dt);
+        hunter.BorderRestriction(800., 600.);
+        hunter.SpeedRestriction(100);
+        hunter.aggiornaSprite();
+
+        // render
+        window.clear(sf::Color(15, 17, 26)); // blu notte
+        for (auto &b : stormo.flock) {
+          dot.setPosition(b.getPosition().x, b.getPosition().y);
+          // b.draw(window);
+          window.draw(dot);
+        }
+        dot.setPosition(hunter.getPosition().x, hunter.getPosition().y);
+        window.draw(dot);
+
+        window.display();
       }
     }
 
-    /*for (auto &x : stormo.flock) {
-      Coords ruled_velocity =
-          update_Velocity_with_rules_for_single_boid(params, stormo, x);
-      x.update_Velocity_with_rules_for_single_boid(ruled_velocity);
-      x.update_Position_in_time_for_single_boid(dt);
-      x.applyBorderRestriction(1800., 1600.);
-      x.aggiornaSprite(); */
-    // problema di architettura del loop, i boids si aggiornano con le
-    // velocità e poszioni future dello stormo, anzichè reagire tuti a quelle
-    // presenti?
-    std::vector<Coords> updated_velocities{};
-    for (auto &x : stormo.flock) {
-      Coords new_velocity =
-          create_velocity_with_rules_for_single_boid(params, stormo, x, hunter);
-      updated_velocities.push_back(new_velocity);
-    }
-    for (long unsigned int i = 0; i != stormo.flock.size(); ++i) {
-      stormo.flock[i].update_Velocity_with_rules_for_single_boid(
-          updated_velocities[i]);
-      stormo.flock[i].update_Position_in_time_for_single_boid(dt);
-      stormo.flock[i].BorderRestriction(800., 600.);
-      stormo.flock[i].SpeedRestriction(130);
-      stormo.flock[i].aggiornaSprite();
-    }
+    sf::Event event2;
+    while (window2.pollEvent(event2)) {
+      if (event2.type == sf::Event::Closed) {
+        window2.close();
+      } else {
+        double dt2 = clock.restart().asSeconds();
+        Coords mean_speed_for_cycle = mean_velocity(stormo);
+        vector_of_mean_speeds.push_back(mean_speed_for_cycle);
 
-    Coords new_hunt_velocity = hunt(hunter, stormo, params);
-    hunter.update_Velocity_with_rules_for_single_boid(new_hunt_velocity);
-    hunter.update_Position_in_time_for_single_boid(dt);
-    hunter.BorderRestriction(800., 600.);
-    hunter.SpeedRestriction(100);
-    hunter.aggiornaSprite();
+        window2.clear();
 
-    // render
-    window.clear(sf::Color(15, 17, 26));  // blu notte
-    for (auto &b : stormo.flock) {
-      dot.setPosition(b.getPosition().x, b.getPosition().y);
-      // b.draw(window);
-      window.draw(dot);
+        window2.draw(axis);
+        for (auto &s : vector_of_mean_speeds) {
+          double output_speed = std::sqrt(std::pow(s.x, 2) + std::pow(s.y, 2));
+          point.setPosition(dt2 + 50.f, output_speed);
+          window2.draw(point);
+        }
+        window2.display();
+      }
     }
-    dot.setPosition(hunter.getPosition().x, hunter.getPosition().y);
-    window.draw(dot);
-    window.display();  // fai vedere la finestra
   }
-  return 0;
 }
--- statistics.cpp
+++ statistics.cpp
@@ -1,30 +1,55 @@
 #include "stats.cpp"
+#include <cmath>
 
 using namespace boidz;
 
-int main()
-{
-    
+void speed_stats(Flock const& stormo, double const &time, sf::RenderWindow &window2) {
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Statistics");
-    window.setFramerateLimit(60);
+  float margine_x = 50.f;
+  float margine_y = 50.f;
 
-    sf::VertexArray velocity_graphic(sf::PrimitiveType::Lines, 4);
-    sf::Color axis_color = sf::Color::White;
+  sf::CircleShape point(2.f);
+  point.setFillColor(sf::Color::Red);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
+  sf::Vector2f origin(margine_x, 600.f - margine_y);
 
-            window.draw(velocity_graphic);
-            window.display();
-        }
-        return 0;
+  sf::VertexArray axis(sf::PrimitiveType::Lines, 4);
+  sf::Color axis_color = sf::Color::White;
+
+  axis[0].position = origin;
+  axis[0].color = axis_color;
+  axis[1].position = sf::Vector2f(800.f - 50.f, origin.y);
+  axis[1].color = axis_color;
+
+  axis[2].position = origin;
+  axis[2].color = axis_color;
+  axis[3].position = sf::Vector2f(origin.x, 50.f);
+  axis[3].color = axis_color;
+
+  sf::Clock clock;
+
+  std::vector<Coords> vector_of_mean_speeds{};
+
+  while (window2.isOpen()) {
+
+
+    sf::Event event;
+    while (window2.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window2.close();
+      }
+      Coords mean_speed_for_cycle = mean_velocity(stormo);
+      vector_of_mean_speeds.push_back(mean_speed_for_cycle);
+
+      for(auto &s : vector_of_mean_speeds){
+         double output_speed = std::sqrt(std::pow(s.x, 2) + std::pow(s.y, 2));
+         point.setPosition(time + 50.f, output_speed);
+      }
+      window2.clear();
+
+      window2.draw(axis);
+      window2.draw(point);
+      window2.display();
     }
+  }
 }
