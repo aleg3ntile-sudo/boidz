f6b89a81d2b283c9e3b8f646b1ec008052d25753
Fra <francesco.chesani@gmail.com>
2026-06-01 21:14:31 +0200

    Resolved conflicts

--- stats.hpp
+++ stats.hpp
@@ -10,7 +10,6 @@ float mean_speed(Flock const &f);
 std::vector<float> get_vector_of_speeds(Flock const &f);
 std::vector<float> get_vector_of_distances(Flock const &f);
 float mean_distance(Flock const &f);
-Coords centre_of_mass_position(Flock const &f);
 float standard_deviation(std::vector<float> const &v);
 float standard_deviation(std::vector<SingleBoid> const &v);
 
