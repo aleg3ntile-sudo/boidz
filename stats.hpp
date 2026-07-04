373a3e9f0192ec41029cd7dadf86c502c2ab0c74
Fra <francesco.chesani@gmail.com>
2026-07-04 12:16:10 +0200

    formatting problem resolved

--- stats.hpp
+++ stats.hpp
@@ -12,7 +12,7 @@ namespace boidz
         std::vector<float> vector_of_errors_speed{};
         std::vector<float> vector_of_errors_distance{};
     };
-    
+
     std::vector<float> get_vector_of_speeds(Flock const &f);
     std::vector<float> get_vector_of_distances(Flock const &f);
     Coords mean_and_error(const Flock &f, const std::string &type);
